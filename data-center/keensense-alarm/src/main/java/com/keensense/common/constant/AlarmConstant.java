1e949927fdd39ba2b823aa3344403af5395a5386
shayne007 <shayne007@sina.cn>
2025-08-19 22:11:21 +0800

    [refactor] thread pool usage

---
+++ data-center/keensense-alarm/src/main/java/com/keensense/common/constant/AlarmConstant.java
@@ -0,0 +1,27 @@
+package com.keensense.common.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author ycl
+ * @date 2019/5/24
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AlarmConstant {
+
+    public static final int MOTOR_VEHICLE = 2;
+
+
+    public static final int THREAD_POOL_NUMBER = 6;
+
+
+    /**
+     * 陌生人布控
+     */
+    public static final int CONTROL_STRANGER = 3;
+
+
+    public static final int RECEIVE_MESSAGE_SIZE = 100;
+
+}
