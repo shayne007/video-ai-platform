81ec75082893d6b9bb793966770d640fb928a76a
shayne007 <shayne007@sina.cn>
2025-09-02 23:23:33 +0800

    [refactor] thread pool usage

---
+++ task-manager/src/main/java/com/keensense/task/mapper/CameraAnalysisTrackHisMapper.java
@@ -0,0 +1,17 @@
+package com.keensense.task.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.keensense.task.entity.CameraAnalysisTrackHis;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @ClassName: CameraAnalysisTrackHisMapper
+ * @Description: CameraAnalysisTrackHis Mapper
+ * @Author: cuiss
+ * @CreateDate: 2020/4/14 16:01
+ * @Version: 1.0
+ * @Company: 长沙千视通智能科技有限公司
+ */
+@Mapper
+public interface CameraAnalysisTrackHisMapper extends BaseMapper<CameraAnalysisTrackHis> {
+}
